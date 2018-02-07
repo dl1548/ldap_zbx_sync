b941b89d5af262036b31f7f5fa0e91fee8a3f59f
gitlizili <dl1548@sina.com>
2018-02-07 17:45:55 +0800

    modify get_ldap
    获取ouGUID。可使用UUID转换

--- moudle/get_ldap.py
+++ moudle/get_ldap.py
@@ -20,7 +20,17 @@ class LdapToZbx():
         except:
 #            self.ldap_error = 'Connect to %s failed, Error:%s.' %(ldap_url,err.message['desc'])
             return 'connect ldap error'
-
+    def get_ou_guid(self,baseDN):
+        searchFilter = '(&(objectClass=OrganizationalUnit))'
+        results = self.con.search_s(baseDN,ldap.SCOPE_SUBTREE,searchFilter,['distinguishedName','objectGUID'])
+        #return results
+        for i in results:
+            if i[0] == baseDN:
+                return i[1]['objectGUID']
+                        
+    #SCOPE_BASE (基数：查询指定DN，也就是在DN中指定的那个，就只查这DN的)
+    #SCOPE_ONELEVEL (一级：查询指定DN下的一级子目录，不会查子目录的子目录)
+    #SCOPE_SUBTREE (子树：查询指定DN下的所有目录，包括指定DN) 
     def search_ou_user(self,baseDN):
         user_list=[]
         searchFilter = '(&(objectClass=person))' #只查找objectClass=person的
